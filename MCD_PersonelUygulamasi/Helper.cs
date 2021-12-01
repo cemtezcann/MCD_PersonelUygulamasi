2038242972236363a5c92fbf01dca675a81b82fc
cmtzc <tezcancem92@gmail.com>
2021-12-01 16:12:38 +0300

    Add project files.

---
+++ MCD_PersonelUygulamasi/Helper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_PersonelUygulamasi
+{
+    public static class Helper
+    {
+
+        //Helper H1 = new Helper(); !!!Statik olarak işaretlenmiş classlar örneklenemez...
+
+        /* public void Test()
+         {
+        Statik olan class lar içerisinde standart metotlar kullanılmaz.Static olmayan metotlar
+         }
+        */
+
+        /* puclic string Tanim { get; set; }
+
+         Statik olan class lar içerisiidne static olmayan field tanımları yapılamaz.*/
+
+        /* puclic Helper ()
+        {
+        HEr nesne örneği alındığında uygulama standart yapıcı metotu çalıştırır.
+        */
+
+        static Helper()
+        {
+            // Uygulama içerisinde istenildiğinde kadar çağrılsınlar sadece ilk çağrılmada çalışırlar ..
+            Console.WriteLine("Helper => static CTOR");
+        }
+
+        public static void emailGonder(string aliciEmailAdres,string konu,string icerik)
+        {
+            ///Email gönderme işlemleri devam edecek 
+            Console.WriteLine("Mail gönderim işlemi başarılı ");
+        }
+    }
+}
